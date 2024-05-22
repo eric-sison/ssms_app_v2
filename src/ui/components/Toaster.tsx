c022ef3a942b0b514ec145955ae8ae5c12aecd76
Eric Sison <ericsison.dev@gmail.com>
2024-05-22 14:13:46 +0800

    segregated toast component and added hooks folder inside ui

--- src/ui/components/Toaster.tsx
+++ src/ui/components/Toaster.tsx
@@ -8,7 +8,7 @@ import {
   ToastTitle,
   ToastViewport,
 } from "./Toast";
-import { useToast } from "./use-toast";
+import { useToast } from "../hooks/use-toast";
 
 export function Toaster() {
   const { toasts } = useToast();
